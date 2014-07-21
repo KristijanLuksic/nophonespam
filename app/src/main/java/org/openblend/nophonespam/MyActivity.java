6b1167483325c05ff22726e36df2f75151a8627c
KristijanLuksic <kikoluksic@gmail.com>
2014-07-21 14:43:31 +0200

    My first app commit

---
+++ app/src/main/java/org/openblend/nophonespam/MyActivity.java
@@ -0,0 +1,58 @@
+package org.openblend.nophonespam;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyActivity extends Activity {
+    private final String TAG = "My Activity";
+    public final static String EXTRA_MESSAGE = "com.luksic.kristijan.newapplication";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+       public void sendMessage (View view) {
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        String message = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }else if (id==R.id.exit) {
+
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
